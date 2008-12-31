07fa792b4aff1ee4d0c0f1d16f9a39490a305dd4
jagannathRD <jrdindoor@gmail.com>
2009-01-01 03:10:08 +0530

    modifiers

---
+++ AccessModifier/src/Dog.java
@@ -0,0 +1,11 @@
+
+public class Dog {
+
+	int age;
+	String breed;
+
+	Dog(String inbreed) {
+		System.out.println("1-param");
+	}
+
+}
---
+++ AccessModifier/src/DogTester.java
@@ -0,0 +1,14 @@
+
+public class DogTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog dog1 = new Dog("German Shephard");
+		dog1.age = 5;
+		dog1.breed = "lab";
+
+		System.out.println("Dog breed " + dog1.breed);
+		System.out.println("Dog age " + dog1.age);
+	}
+
+}
