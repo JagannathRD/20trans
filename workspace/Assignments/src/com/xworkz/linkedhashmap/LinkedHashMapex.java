ac1afb7206c04c0a9f3647b6e2ee710378fcb99b
jagannathRD <jrdindoor@gmail.com>
2020-09-06 09:04:40 +0530

    linkedhashmap

---
+++ workspace/Assignments/src/com/xworkz/linkedhashmap/LinkedHashMapex.java
@@ -0,0 +1,21 @@
+package com.xworkz.linkedhashmap;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapex {
+
+	public static void main(String[] args) {
+		System.out.println("mms");
+		LinkedHashMap<Integer, String> lHM=new LinkedHashMap();
+		lHM.put(560040, "Vijayanagar");
+		lHM.put(560042, "Rajajinagar");
+		lHM.put(580001,"J P Nagar");
+		
+		System.out.println(lHM);
+		
+		
+		System.out.println("mme");
+
+	}
+
+}
