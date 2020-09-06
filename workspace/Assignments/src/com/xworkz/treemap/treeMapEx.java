acfc2bbd2fd0c13f6e6b5f0281c10a9a4e329009
jagannathRD <jrdindoor@gmail.com>
2020-09-06 09:08:50 +0530

    treemap

---
+++ workspace/Assignments/src/com/xworkz/treemap/treeMapEx.java
@@ -0,0 +1,18 @@
+package com.xworkz.treemap;
+
+import java.util.TreeMap;
+
+public class treeMapEx {
+
+	public static void main(String[] args) {
+		System.out.println("mms");
+		TreeMap<String, Integer> monthDays=new TreeMap<>();
+		monthDays.put("Jan", 31);
+		monthDays.put("Feb", 28);
+		monthDays.put("March", 31);
+		
+		System.out.println(monthDays);
+		System.out.println("mme");
+	}
+
+}
