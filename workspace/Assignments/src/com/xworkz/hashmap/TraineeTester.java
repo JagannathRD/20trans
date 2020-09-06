0ff8fce1edff0897805f053a9f715c0984edc2ea
jagannathRD <jrdindoor@gmail.com>
2020-09-06 08:57:17 +0530

    hashmap

---
+++ workspace/Assignments/src/com/xworkz/hashmap/TraineeTester.java
@@ -0,0 +1,21 @@
+package com.xworkz.hashmap;
+
+import java.util.LinkedHashMap;
+
+public class TraineeTester {
+
+	public static void main(String[] args) {
+		System.out.println("mms");
+		LinkedHashMap<Trainee, Integer> trainee = new LinkedHashMap<>();
+		trainee.put(new Trainee(1, "Krupa"), 20);
+		trainee.put(new Trainee(2, "Suva"), 15);
+		trainee.put(new Trainee(1, "Krupa"), 20);
+		trainee.put(new Trainee(3, "Roopa"), 5);
+
+		System.out.println(trainee);
+
+		System.out.println("mme");
+
+	}
+
+}
